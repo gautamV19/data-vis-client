a8c9ae731920feed036578dd2cd8910f8fc3a05c
gautamV19 <ee20b144@smail.iitm.ac.in>
2022-08-06 18:04:25 +0530

    date-change

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 function App() {
-  const startDate = new Date();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
     <Grid container sx={{ padding: 3 }}>
       <Grid container spacing={2}>
@@ -17,18 +19,20 @@ function App() {
             showBorder
             onChange={(date) => {
               console.log(date);
+              setStartDate(date);
             }}
             placeholder={"Select a Start Date date"}
           />
         </Box>
         <Box sx={{ height: "100px", width: "100px" }}>
           <ModernDatepicker
-            date={startDate}
+            date={endDate}
             format={"DD-MM-YYYY"}
             label="End Date"
             showBorder
             onChange={(date) => {
               console.log(date);
+              setEndDate(date);
             }}
             placeholder={"Select a End Date date"}
           />
